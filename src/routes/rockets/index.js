e497e6d54adbca4588e17ac30d9b26cfe454663f
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-03-08 13:30:51 +0530

    Added without cache route

--- src/routes/rockets/index.js
+++ src/routes/rockets/index.js
@@ -2,24 +2,65 @@ const express = require('express')
 const { GET_CACHE, SET_CACHE, http } = require('../../helpers')
 const router = express.Router()
 
-router.get('/list', async (_, res) => {
+router.get('/list/withoutCache', async (_, res) => {
   try {
-    const cachedResponse = await GET_CACHE('rockets')
-    if (cachedResponse) {
-      res.status(200).json(JSON.parse(cachedResponse))
-      return
+    const respone = await http.get('rockets')
+    res.status(200).json(respone.data)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.get('/list', async (req, res) => {
+  try {
+    if (req.query.cache != 'false') {
+      const cachedResponse = await GET_CACHE('rockets')
+      if (cachedResponse) {
+        res.status(200).json(JSON.parse(cachedResponse))
+        return
+      }
     }
     const respone = await http.get('rockets')
-    await SET_CACHE(
+    (req.query.cache != 'false') && await SET_CACHE(
       'rockets',
       JSON.stringify(respone.data),
       'EX',
       900
     )
-    res.status(200).send(respone.data)
+    res.status(200).json(respone.data)
   } catch (err) {
     res.status(500).send(err.message)
   }
 })
+
+router.get('/:rocketId/withoutCache', async (req, res) => {
+  try {
+    const respone = await http.get(`rockets/${req.params.rocketId}`)
+    res.status(200).json(respone.data)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.get('/:rocketId', async (req, res) => {
+  try {
+    const cachedResponse = await GET_CACHE(`rocket_${req.params.rocketId}`)
+    if (cachedResponse) {
+      res.status(200).json(JSON.parse(cachedResponse))
+      return
+    }
+    const respone = await http.get(`rockets/${req.params.rocketId}`)
+    await SET_CACHE(
+      `rocket_${req.params.rocketId}`,
+      JSON.stringify(respone.data),
+      'EX',
+      900
+    )
+    res.status(200).json(respone.data)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
 
 module.exports = router
